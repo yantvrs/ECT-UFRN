afb17893e50e1298babac7ceae85fbc4649dbf5e
yantavares2002 <yantavares2002@gmail.com>
2022-05-14 15:43:59 -0300

    Montando funções

--- Escultura_3D_Cpp/sculptor.cpp
+++ Escultura_3D_Cpp/sculptor.cpp
@@ -54,3 +54,61 @@ void Sculptor::setColor(float r, float g, float b, float alpha){
     this -> b = b;
     this -> a = alpha;
 }
+
+//Put voxel
+void Sculptor::putVoxel(int x, int y, int z){
+    this -> vox[x][y][z].r = this -> r;
+    this -> vox[x][y][z].g = this -> g;
+    this -> vox[x][y][z].b = this -> b;
+    this -> vox[x][y][z].a = this -> a;
+    this -> vox[x][y][z].isOn = true;
+}
+
+//Adicionando paralelepípedo
+void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
+
+    //Verificador das dimensões
+    x0 = ( x0 < 0 ) ? 0:x0;
+    x1 = ( x1 > this->nx ) ? this -> nx:x1;
+    y0 = ( y0 < 0 ) ? 0:y0;
+    y1 = ( y1 > this->ny ) ? this -> ny:y1;
+    z0 = ( z0 < 0 ) ? 0:z0;
+    z1 = ( z1 > this->nz ) ? this -> nz:z1;
+
+    //Atribuição dos intervalos nos voxels
+    for( int i = x0; i < x1; i++){
+        for( int j = y0; j < y1; j++){
+            for( int k = z0; k < z1; k++){
+                this -> putVoxel(i,j,k);
+            }
+        }
+    }
+}
+
+//Removendo paralelepípedo
+void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
+    for(int i = x0; i < x1; i++){
+        for(int j = y0; j < y1; j++){
+            for(int k = z0; k < z1 ; k++){
+                this -> cutVoxel(i,j,k);
+            }
+        }
+    }
+}
+
+//Adicionando esfera
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
+    putEllipsoid(xcenter, ycenter, zcenter, radius, radius, radius);
+}
+
+//Removendo esfere
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
+    cutEllipsoid(xcenter, ycenter, zcenter, radius, radius, radius);
+}
+
+//Adicionando elipsóide
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, rz){
+
+    //Verificador das dimensões
+    int xo = (xcenter - rx < 0) ? 0:
+}
